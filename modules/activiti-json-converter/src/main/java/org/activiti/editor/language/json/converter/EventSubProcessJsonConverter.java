30e957c552d2c36ce9def74127ec220032e04a12
Alexey Kakunin <akakunin@gmail.com>
2013-05-05 23:22:28 +0400

    Event sub-process in case included into another subprocess - in that case nested elements calculated not from subprocess - but from parent subprocess (look at Travel Booking.bpmn from examples)

--- modules/activiti-json-converter/src/main/java/org/activiti/editor/language/json/converter/EventSubProcessJsonConverter.java
+++ modules/activiti-json-converter/src/main/java/org/activiti/editor/language/json/converter/EventSubProcessJsonConverter.java
@@ -54,7 +54,7 @@ public class EventSubProcessJsonConverter extends BaseBpmnJsonConverter {
     ArrayNode subProcessShapesArrayNode = objectMapper.createArrayNode();
     GraphicInfo graphicInfo = model.getGraphicInfo(flowElement.getId());
     processor.processFlowElements(subProcess.getFlowElements(), model, subProcessShapesArrayNode, 
-        graphicInfo.getX() + subProcessX, graphicInfo.getY() + subProcessY);
+        graphicInfo.getX(), graphicInfo.getY());
     flowElementNode.put("childShapes", subProcessShapesArrayNode);
   }
   
